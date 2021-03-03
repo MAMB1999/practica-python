ba27080597e5a5dde659aab16f6fc95ff3722c3c
MAMB1999 <74522304+MAMB1999@users.noreply.github.com>
2021-03-03 00:10:23 -0300

    segundo commit

--- Practica_general/Datetime.py
+++ Practica_general/Datetime.py
@@ -6,5 +6,5 @@ print(date(1876,5,7)) #Asigna e imprime la fecha asignada
 print(date.fromtimestamp(1526244370)) # Asigna una marca de tiempo que tiene como referencia el 1\1\1970
 
 fecha_hoy = date.today()
-print("Hoy ",fecha_hoy.day,"\nMes: ",fecha_hoy.month,"\nAno:",fecha_hoy.year)
+print("Hoy ",fecha_hoy.day,"\nMes: ",fecha_hoy.month,"\nAnno:",fecha_hoy.year)
 
